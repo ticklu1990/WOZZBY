51935d4ec2c3671d8b8a3d735ca0c0bebb669ae8
unknown <rajatsngpt7@gmail.com>
2020-08-26 23:04:35 +0530

    Add Rexux to Details

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ import data from "./data";
 const app = express();
 
 //end point,path of this endpoint:
+
+//id as a parameter will go to this route:
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find((x) => x._id === productId);
+  if (product) res.send(product);
+  else res.status(404).send({ msg: "Product Not Found" });
+});
+
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
