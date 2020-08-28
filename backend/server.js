0fcde7162a2b1a2eadb27a04092770f50aecd701
unknown <rajatsngpt7@gmail.com>
2020-08-28 22:40:09 +0530

    Qty to cart[BugFixed]

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,6 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
+app.listen(6000, () => {
+  console.log("Server started at http://localhost:6000");
 });
